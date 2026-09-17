7228fec3e884fe32356d0d995cde4206b1b45c31
Cho Zhi Xian <2603724@sit.singaporetech.edu.sg>
2026-09-17 21:46:13 +0800

    Added all function listed (w/o) code yet.

--- modular_auditor.py
+++ modular_auditor.py
@@ -28,10 +28,40 @@ while True:
             total_inventory -= quantity  # Revert the addition
             break
         else:    
+
             print(f"Added {quantity} units. Total inventory is now {total_inventory}.")
+
 
 #Final summary of the audit
 print("\nInventory Audit Summary")
 print("----------------------")
 print(f"Total inventory: {total_inventory} units")
 print(f"Failed entries: {failed_entries}")
+
+
+"""Handles the prompt, handles input validation, and
+returns a valid integer or a "quit" signal"""
+def get_valid_input():
+  print("Hello from a function")
+
+
+"""Calculates the new total and
+returns it"""
+def process_delivery(current_total, new_value):
+    return True
+
+
+"""A new requirement! This function takes a delivery
+amount and returns the tax (10% of that specific delivery)"""
+def calculate_tax(amount):
+    return True
+
+"""A dedicated function to print
+the final summary """
+def generate_report(total_units, failed_attempts):
+    return True
+
+
+#Template
+# def my_function():
+#   print("Hello from a function")
